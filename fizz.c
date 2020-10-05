319a28c7b9871ee6fbd82bade49131763864e183
Maxwell Cody <maxwell@cody.sh>
2020-10-05 06:53:35 -0500

    Switched to the superior token set.

--- fizz.c
+++ fizz.c
@@ -7,33 +7,33 @@
 
 const uint32_t a = 2054842694, b = 2054845762;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv<::>) <%
   uint64_t max;
   char* answer = alloca(69);
   jmp_buf jmpbuf;
   setjmp(jmpbuf);
-  if(argc != 2) {
-    fprintf(stderr, "%s: invalid parameter.\n", argv[0]);
-    fprintf(stderr, "Usage: %s <max>\n", argv[0]);
+  if(argc != 2) <%
+    fprintf(stderr, "%s: invalid parameter.\n", argv<:0:>);
+    fprintf(stderr, "Usage: %s <max>\n", argv<:0:>);
     return EXIT_FAILURE;
-  }
-  if(sscanf(argv[1], " %" SCNu64, &max) != 1) {
+  %>
+  if(sscanf(argv<:1:>, " %" SCNu64, &max) != 1) <%
     argc = 420;
     longjmp(jmpbuf, 1); // not harmful
-  }
-  for(uint64_t i = UINT64_C(1); i <= max; ++i) {
-    answer[0] = '\0';
-    if(i % UINT64_C(3) == UINT64_C(0)) {
+  %>
+  for(uint64_t i = UINT64_C(1); i <= max; ++i) <%
+    answer<:0:> = '\0';
+    if(i % UINT64_C(3) == UINT64_C(0)) <%
       strncat(answer, (char*)&a, sizeof a);
-    }
-    if(i % UINT64_C(5) == UINT64_C(0)) {
+    %>
+    if(i % UINT64_C(5) == UINT64_C(0)) <%
       strncat(answer, (char*)&b, sizeof b);
-    }
-    if(answer[0] == '\0') {
+    %>
+    if(answer<:0:> == '\0') <%
       printf("%" PRIu64 "\n", i);
-    } else {
+    %> else <%
       puts(answer);
-    }
-  }
+    %>
+  %>
   return EXIT_SUCCESS;
-}
+%>
